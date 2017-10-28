c313849b9ada599443275316231e9a3c328f76db
gurre <gsvalander@gmail.com>
2017-10-28 12:27:03 +0200

    Added package.json

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import createEventualAction from './createEventualAction';
+import createEventualReducer from './createEventualReducer';
+import lseq from './lseq';
+
+export { createEventualAction, createEventualReducer, lseq };
